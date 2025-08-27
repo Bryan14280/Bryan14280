7a9ba9daaf27b5775b0e19190558daf3a9e40a93
Bryan14280 <142761623+Bryan14280@users.noreply.github.com>
2025-08-27 15:48:00 -0300

    Update AimbotMelhoradoProplayer.lua

--- AimbotMelhoradoProplayer.lua
+++ AimbotMelhoradoProplayer.lua
@@ -1,4 +1,4 @@
---Enjoy BusWare!
+-- Aproveita Jovém !
   local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
     local Window = Library.CreateLib("BusWare", "Sentinel")
     
